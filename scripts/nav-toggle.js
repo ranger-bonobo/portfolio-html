08a375ce66806b94ba91755b0909d09a7086131d
ranger-bonobo <nghoting88@gmail.com>
2024-11-28 18:59:55 +0800

    added headers and section layouts

---
+++ scripts/nav-toggle.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navButton = document.querySelector(".nav-button");
+  const navList = document.querySelector(".nav-list");
+  const header = document.querySelector(".header");
+  const navItems = document.querySelectorAll(".nav-item a");
+
+  navButton.addEventListener("click", function () {
+    if (navList.classList.contains("active")) {
+      navList.style.height = "0";
+      navList.addEventListener(
+        "transitionend",
+        function () {
+          navList.classList.remove("active");
+        },
+        { once: true }
+      );
+    } else {
+      navList.classList.add("active");
+      navList.style.height = navList.scrollHeight + "px";
+    }
+    navButton.classList.toggle("cross");
+  });
+
+  document.addEventListener("click", function (event) {
+    if (
+      !header.contains(event.target) &&
+      !navList.contains(event.target) &&
+      navList.classList.contains("active")
+    ) {
+      navList.style.height = "0";
+      navList.addEventListener(
+        "transitionend",
+        function () {
+          navList.classList.remove("active");
+        },
+        { once: true }
+      );
+      navButton.classList.toggle("cross");
+    }
+  });
+
+  navItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      navList.style.height = "0";
+      navList.addEventListener(
+        "transitionend",
+        function () {
+          navList.classList.remove("active");
+        },
+        { once: true }
+      );
+      navButton.classList.remove("cross");
+    });
+  });
+});
